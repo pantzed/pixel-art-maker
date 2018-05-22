24fc37fa43783fe53eba34e7779d144a513267fa
Edward <edpantzar@gmail.com>
2018-05-22 15:54:07 -0500

    Flood fill now fills empty canvas

--- pixel-art.js
+++ pixel-art.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector('section[id="canvas-area"]').appendChild(square);
         }
     }
-    addPixels(5);
+    addPixels(10);
 
     function buildColorPalette() {
         let paletteArea = document.querySelector('section[id="palette-area"]');
@@ -83,38 +83,50 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function startFloodFill() {
         let startingIndex = (Array.from(event.currentTarget.childNodes).indexOf(event.target));
+        console.log(Array.from(event.currentTarget.childNodes));
         floodFill(startingIndex);
         return;
         }
 
     function floodFill(currentIndex){
-        let north = event.currentTarget.childNodes[currentIndex - 20];
+        let north = event.currentTarget.childNodes[currentIndex - 10];
         let east = event.currentTarget.childNodes[currentIndex + 1];
-        let south = event.currentTarget.childNodes[currentIndex + 20];
+        let south = event.currentTarget.childNodes[currentIndex + 10];
         let west = event.currentTarget.childNodes[currentIndex - 1];
-        console.log(north, east, south, west);
-        if (north.style.backgroundColor === startingColor) {
-            north.style.backgroundColor = brushColor;
-            north.style.borderColor = brushColor
-            floodFill(currentIndex - 20);
+        if (currentIndex - 10 > 2) {
+            if (north.style.backgroundColor === startingColor) {
+                north.style.backgroundColor = brushColor;
+                north.style.borderColor = brushColor;
+                console.log(currentIndex - 10);
+                floodFill(currentIndex - 10);
+            }
         }
-        if (east.style.backgroundColor === startingColor){
-            east.style.backgroundColor = brushColor;
-            east.style.borderColor = brushColor
-            floodFill(currentIndex + 1);
+        if (currentIndex + 1 < 83){
+            if (east.style.backgroundColor === startingColor){
+                east.style.backgroundColor = brushColor;
+                east.style.borderColor = brushColor
+                console.log(currentIndex + 1);
+                floodFill(currentIndex + 1);
+            }
         }
-        if (south.style.backgroundColor === startingColor){
-            south.style.backgroundColor = brushColor;
-            south.style.borderColor = brushColor
-            floodFill(currentIndex + 20)
+        if (currentIndex + 10 < 83){
+            if (south.style.backgroundColor === startingColor){
+                south.style.backgroundColor = brushColor;
+                south.style.borderColor = brushColor;
+                console.log(currentIndex + 10);
+                floodFill(currentIndex + 10)
+            }
         }
-        if (west.style.backgroundColor === startingColor){
-            west.style.backgroundColor = brushColor;
-            west.style.borderColor = brushColor
-            floodFill(currentIndex - 1);
+        if (currentIndex - 1 > 2){
+            if (west.style.backgroundColor === startingColor){
+                west.style.backgroundColor = brushColor;
+                west.style.borderColor = brushColor;
+                console.log(currentIndex - 1);
+                floodFill(currentIndex - 1);
+            }
         }
         else {
-            return ;
+            return;
         }    
     }
 });
